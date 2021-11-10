74f35b2f3c9e43b39bee888d4f36f55c4718203a
TylerJang <tjang006@ucr.edu>
2021-11-09 16:11:15 -0800

    Broke classifier comparison again 3

--- scripts_for_auto_cell_count/audit.py
+++ scripts_for_auto_cell_count/audit.py
@@ -14,6 +14,7 @@ import random
 import shutil
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 # Method to change working directory from inputted ImageJ Macro
 currDir = os.getcwd()
@@ -25,11 +26,16 @@ setDir(sys.argv[1])
 # Get the selected classifier by the user
 selectedClassifier = sys.argv[2]
 =======
+=======
+>>>>>>> parent of dc34ec3... 11/9/2021 stuck on no cells in audit(and i expect validation) means no roi, messing up count over dir and the like
 ##REMOVE THIS LATER
 #os.chdir("F:/Theo/full_backup_3_23_2021/Kaul_lab_work/bin_general/scripts_for_auto_cell_count/")
 os.chdir("C:/Users/19099/Documents/Kaul_Lab/AutoCellCount/Automatic-Cell-counting-with-TWS/scripts_for_auto_cell_count")
 selectedClassifier = "classifier2"
 
+<<<<<<< HEAD
+>>>>>>> parent of dc34ec3... 11/9/2021 stuck on no cells in audit(and i expect validation) means no roi, messing up count over dir and the like
+=======
 >>>>>>> parent of dc34ec3... 11/9/2021 stuck on no cells in audit(and i expect validation) means no roi, messing up count over dir and the like
 #read in genotype.csv
 geno_file = "../training_area/testing_area/geno_full.csv"
