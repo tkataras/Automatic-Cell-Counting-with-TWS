3f49f6054f40422ba183eaa614f6b1aaf1e1311b
TylerJang <tjang006@ucr.edu>
2021-11-15 13:29:30 -0800

    Updating audit.py to match geno to images, and start doing statistical calculations:

--- scripts_for_auto_cell_count/audit.py
+++ scripts_for_auto_cell_count/audit.py
@@ -25,14 +25,13 @@ selectedClassifier = sys.argv[2]
 #read in genotype.csv
 geno_file = "../training_area/testing_area/geno_full.csv"
 
-
 geno = pd.read_csv(geno_file)
 # Get the unique genotype labels
 lvl_geno = np.unique(geno)
 if len(lvl_geno) != 2:
     print("automatic analysis can only be done with 2 levels, for alterative analysis use _Final.csv files in classifier folders")
 
-#read in file names from the counted/projected experimental dataset
+# Read in file names from the counted/projected experimental dataset
 folder_loc = "../training_area/testing_area/Weka_Output_Counted/" +  selectedClassifier
 files = []
 for image in os.listdir(folder_loc):
@@ -41,53 +40,58 @@ for image in os.listdir(folder_loc):
 
 ####TODO make rest of this work, i am this far
 
-#determine number of draws by number of files in validation hand count folder
+# Determine number of draws by number of files in validation hand count folder
 val_loc = "../training_area/Validation_Hand_Counts/"
 val_files = os.listdir(val_loc)
 draws = len(val_files)
 draws_per_geno = int(draws/len(lvl_geno))
 
-#select the files for each genotype
-#define experimental variable level 1 files
-ev0_files = []
+# Select the files for each genotype
+# Define experimental variable level 1 files
+ev0_files = {}
 for i in range(len(files)):
     if geno["geno"][i] == lvl_geno[0]:
-        ev0_files.append(files[i])
+        ev0_files[files[i]] = lvl_geno[0]
             
 #define experimental variable level 2 files        
-ev1_files = []
+ev1_files = {}
 for i in range(len(files)):
     if geno["geno"][i] == lvl_geno[1]:
-        ev1_files.append(files[i])
+        ev1_files[files[i]] = lvl_geno[1]
 
+print(ev0_files)
 #make random selections for level 1
 LEV0 = len(ev0_files)
 LEV1 = len(ev1_files)
 
-rand_ev0 = random.sample(range(0,LEV0), draws_per_geno)
-rand_ev1 = random.sample(range(0,LEV1), draws_per_geno)
+# Randomly select images to be auditted
+# TODO may grab same image twice, I'm not sure
+audit_set = {}
+for incremenet in range(0, draws_per_geno):
+    ev0_key, ev0_value = random.choice(list(ev0_files.items()))
+    ev1_key, ev1_value = random.choice(list(ev1_files.items()))
+    audit_set[ev0_key] = ev0_value
+    audit_set[ev1_key] = ev1_value
 
 ###need to get these random variable numbers from oritional file directory, eg images, counted 
+print(audit_set)
 
-# Build the list of randomly selected images to be auditted
-audit_set = []
-for index in rand_ev0:
-    audit_set.append(ev0_files[index])
-
-for index in rand_ev1:
-    audit_set.append(ev1_files[index])
-
+""" Temp so I don't need to redo ROI stuff
 # Copy selected images into audit images directory
-for file in audit_set:
+for file in audit_set.keys():
     filename =  os.path.basename(file)
     print(filename)
+    
     shutil.copyfile("../training_area/testing_area/images/" + filename, os.path.join("../training_area/testing_area/Audit_Images/" + selectedClassifier +"/", filename))
     shutil.copyfile("../training_area/testing_area/Weka_Output_Counted/" + selectedClassifier +"/" + filename, os.path.join("../training_area/testing_area/Audit_Counted/"+ selectedClassifier +"/", filename))
-
-
-
+"""
 #based on the order of these images, need to set up new genotypes file. 
 #because of randomly selecting images, the geno ratio will be the same, but order will likely not be
 #based on alphabetical file order, as they will be read in by imagej and python
+genoCSV = []
+for key, value in sorted(audit_set.items()):
+    print(key)
+    print(value)
+
 
 print("Finished audit.py")
